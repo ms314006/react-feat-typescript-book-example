940d00e9c0d999a2aded3f9f54b02ca4b46b9e52
GQSM <ms314006@hotmail.com>
2020-10-16 00:53:52 +0800

    remove useless code

--- Ch02-4/src/views/Home/Home.jsx
+++ Ch02-4/src/views/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Counter from '../../components/Counter';
 import SuperAmazingCounter from '../../components/SuperAmazingCounter';
 
