28f11609ac859085a10b956a3728a040a9465f7b
GQSM <ms314006@hotmail.com>
2020-10-15 08:04:39 +0800

    finished ch01 example code

---
+++ Ch01/src/components/HelloWorld/HelloWorld.jsx
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import styles from './index.scss';
+ 
+const HelloWorld = () => ( 
+  <div className={styles.title}>
+    Hello world!
+  </div>  
+);
+ 
+export default HelloWorld;
